2371a26fb8e6b49c3271f71c766c36a114021bc5
Ankur Sardar <ankursardar.work@gmail.com>
2026-08-22 11:11:00 +0530

    feat: learn basic functions

---
+++ Functions/anonymous.js
@@ -0,0 +1,6 @@
+// anonymous function - declared and called using variable without function name
+
+let add = function(a,b){
+    return a+b
+}
+console.log(add(1,2));
---
+++ Functions/namedFunc.js
@@ -0,0 +1,15 @@
+// named function - a regular function with a name
+
+function print_number(){
+    console.log(1,2);
+    
+}
+
+print_number()
+
+function print_name(name){ // a parameter - passed while creating a function
+    console.log(name);
+    
+}
+
+print_name("Ankur") // an argument - passed while calling a function
